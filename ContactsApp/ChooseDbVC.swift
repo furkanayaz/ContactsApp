bc61a28cd0160b20f5a660f4359a254fb3663148
Furkan Ayaz <furkanayaz.dev@gmail.com>
2024-01-31 23:33:11 +0300

    I made the ui screens.

--- ContactsApp/ChooseDbVC.swift
+++ ContactsApp/ChooseDbVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChooseDbVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ ContactsApp/ContactsVC.swift
@@ -0,0 +1,29 @@
+//
+//  ContactsVC.swift
+//  ContactsApp
+//
+//  Created by FURKAN AYAZ on 31.01.2024.
+//
+
+import UIKit
+
+class ContactsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
